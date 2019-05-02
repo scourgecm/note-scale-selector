ab32cd4cc3f1366c14f78587b8c00f83b98e5fdd
Patricio Cornejo <scourgecm@gmail.com>
2019-05-02 17:28:31 -0400

    Base for scale building

--- src/app/model/note-accident.model.ts
+++ src/app/model/note-accident.model.ts
@@ -2,10 +2,11 @@ export interface IAccident {
     id: number;
     name: string;
     symbol: string;
+    value: number;
 }
 
 export const ACCIDENTS: IAccident[] = [
-    { id: 1, name: 'flat', symbol: '&#9837;' },
-    { id: 2, name: 'natural', symbol: '&#9838;' },
-    { id: 3, name: 'sharp', symbol: '&#9839;' },
+    { id: 1, name: 'flat', symbol: '&#9837;', value: -1 },
+    { id: 2, name: 'natural', symbol: '&#9838;', value: 0 },
+    { id: 3, name: 'sharp', symbol: '&#9839;', value: 1 },
 ];
--- src/app/model/note-set.model.ts
+++ src/app/model/note-set.model.ts
@@ -8,7 +8,29 @@ export interface INoteSet {
 }
 
 export const NOTES: string[] = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
-export const MODES: string[] = ['ionian', 'dorian', 'phrygian', 'lydian', 'mixolydian', 'aeolian', 'locrian'];
+export const CHROMATIC: string[] = [
+    'C',
+    'C#/Db',
+    'D',
+    'D#/Db',
+    'E',
+    'F',
+    'F#/Gb',
+    'G',
+    'G#/Ab',
+    'A',
+    'A#/Bb',
+    'B',
+];
+export const MODES: string[] = [
+    'ionian',
+    'dorian',
+    'phrygian',
+    'lydian',
+    'mixolydian',
+    'aeolian',
+    'locrian',
+];
 export const CHORDS: string[] = [
     'maj',
     'min',
